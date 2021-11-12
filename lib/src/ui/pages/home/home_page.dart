eedc0bb3f9895041de050f16bc7c7c57aaf81b5d
Angel Herrarte <angelherrarte3@gmail.com>
2021-11-12 10:54:07 -0600

    Se agregó menú para escoger escenario

--- lib/src/ui/pages/home/home_page.dart
+++ lib/src/ui/pages/home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:fruit_ninja/src/ui/pages/home/home_controller.dart';
@@ -9,17 +10,24 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
   final HomeController _controller = HomeController();
-
+  
   void updateView() => setState(() {});
 
   @override
   void initState() {
+    WidgetsBinding.instance!.addObserver(this);
     SchedulerBinding.instance!.addPostFrameCallback((timeStamp) {
       _controller.init(context, updateView);
     });
     super.initState();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.paused) _controller.handleSoundButton(mute: true);
+    if (state == AppLifecycleState.resumed) _controller.handleSoundButton(mute: false);
   }
 
   @override
@@ -29,37 +37,158 @@ class _HomePageState extends State<HomePage> {
         child: Container(
           width: double.infinity,
           height: double.infinity,
-          color: Colors.brown,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: AssetImage('assets/images/scene-1.gif')
+            ),
+          ),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Text(
-                'Home Page',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.w500,
-                  fontSize: 14,
+              Padding(
+                padding: const EdgeInsets.only(right: 40, top: 20),
+                child: Align(
+                  alignment: Alignment.topRight,
+                  child: _soundButton(),
                 ),
               ),
-              ElevatedButton(
-                onPressed: _controller.goToGame,
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.black,
-                  onPrimary: Colors.white,
-                ),
-                child: const Text(
-                  'JUGAR',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w500,
-                    fontSize: 14,
-                  ),
+              const Spacer(),
+              Transform.translate(
+                offset: const Offset(0, 0),
+                child: Align(
+                  alignment: Alignment.center,
+                  child: _titleText(),
                 ),
               ),
+              const SizedBox(height: 35),
+              _selectScene(),
+              const Spacer(),
+              _creatorName(),
             ],
           ),
         ),
       ),
     );
   }
+
+  Widget _soundButton() {
+    return Container(
+      width: 50,
+      height: 50,
+      decoration: BoxDecoration(
+        image: const DecorationImage(
+          fit: BoxFit.contain,
+          image: AssetImage('assets/images/button-square-background.png'),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.4),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: const Offset(0, 0), // changes position of shadow
+          ),
+        ],
+      ),
+      child: Center(
+        child: ElevatedButton(
+          onPressed: _controller.handleSoundButton,
+          style: ElevatedButton.styleFrom(
+            elevation: 0,
+            padding: const EdgeInsets.all(10),
+            primary: Colors.transparent,
+            onPrimary: const Color(0XFF3A2922),
+          ),
+          child: Image.asset(
+            _controller.isMuted ? 'assets/images/mute-icon.png' : 'assets/images/unmute-icon.png',
+            fit: BoxFit.contain,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _titleText() {
+    return const Text(
+      'Flutter Ninja',
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 48,
+        fontFamily: '8BIT WONDER',
+      ),
+    );
+  }
+
+  Widget _selectScene() {
+    return Transform.translate(
+      offset: const Offset(0, 0),
+      child: Align(
+        alignment: Alignment.center,
+        child: Container(
+          width: 190,
+          decoration: BoxDecoration(
+            image: const DecorationImage(
+              fit: BoxFit.contain,
+              image: AssetImage('assets/images/score-background.png'),
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.2),
+                spreadRadius: 2,
+                blurRadius: 5,
+                offset: const Offset(0, 2), // changes position of shadow
+              ),
+            ],
+          ),
+          child: Center(
+            child: ElevatedButton(
+              onPressed: _controller.showSceneSelectDialog,
+              style: ElevatedButton.styleFrom(
+                elevation: 0,
+                primary: Colors.transparent,
+                onPrimary: const Color(0XFF3A2922),
+                fixedSize: const Size(300, 80),
+                minimumSize: const Size(300, 80),
+              ),
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 2),
+                  child: ZoomIn(
+                    from: 1,
+                    duration: const Duration(milliseconds: 300),
+                    child: const Text(
+                      'Jugar',
+                      style: TextStyle(
+                        color: Color(0XFF3A2922),
+                        fontSize: 20,
+                        fontFamily: '8BIT WONDER',
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _creatorName() {
+    return Padding(
+      padding: const EdgeInsets.only(right: 40, bottom: 20),
+      child: Align(
+        alignment: Alignment.bottomRight,
+        child: const Text(
+          '@ Angel Herrarte',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 12,
+            fontFamily: '8BIT WONDER',
+          ),
+        ),
+      ),
+    );
+
+  }
+
 }
